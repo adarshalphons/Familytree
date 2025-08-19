41ab559a5c92a712c40585321d4dc19e4235def9
Mohan <mohan.oaktree@gmail.com>
2025-08-19 19:46:47 +0530

    Update data

--- src/app/components/FamilyTree/MemberCard.jsx
+++ src/app/components/FamilyTree/MemberCard.jsx
@@ -56,7 +56,7 @@ export default function MemberCard({
 
         {member.name !== "Filtered Results" && (
           <>
-            {(member.id === "V.1") ? "" : <div className={styles.generationRow}>
+            {member.generation !== 0  && <div className={styles.generationRow}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="14"
